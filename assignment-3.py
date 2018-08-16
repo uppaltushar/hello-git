556c410c85718b2283bd35d90378cf685541fea1
uppaltushar <uppal.tushar1998@gmail.com>
2018-08-17 00:38:08 +0530

    update

--- assignment-3.py
+++ assignment-3.py
@@ -4,20 +4,21 @@ x = int(input("enter the the number of elements "))
 for i in range(x):
     list.append(input("enter the values"))
 print(list)
-
+#list append
 #2>
 list1 = ['google','microsoft','tesla','facebook','apple']
 list1.extend(li2)
 print(list1)
-
+#list extend
 #3>
 li3 = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,3,5,6,4,6,4,64,5]
 print(li3.count(1))
-
+#counting in a list
 #4>
 lo = [5,3,6,3,6,3,6,2345,2,426,663,16,31,6,34]
 lo.sort()
 print(lo)
+#sorting of a list
 
 #5>
 list1 = [1,2,3,4,5]
@@ -27,35 +28,46 @@ list1.sort()
 print(list1)
 
 #6>
-#stack using list
-lis = []
+#list with stack
+abc = []
 while(True):
     print("press 1 to add\npress 2 to delete\npress 3 to display\npress 4 to exit")
     option = int(input("enter the option"))
     if(option ==1):
-        lis.append(input("enter the value in stack"))
+        abc.append(input("enter the value in stack"))
     elif(option ==2):
         if len(lis)>0:
-            lis.pop()
+            abc.pop()
         else:
             print("stack is empty")
     elif(option ==3):
-        print(lis)
+        print(abc)
     else:
         break
-lis1 = []
+
+#list with queue
+xyz = []
 while(True):
     print("press 1 to add\npress 2 to delete\npress 3 to display\npress 4 to exit")
     option = int(input("enter the option"))
     if(option ==1):
-        lis1.append(input("enter the value in queue"))
+        xyz.append(input("enter the value in queue"))
     elif(option ==2):
         if len(li)>0:
-            lis1.pop(0)
+            xyz.pop(0)
         else:
             print("queue is empty")
     elif(option ==3):
-        print(lis1)
+        print(xyz)
     else:
         break
-
+#optional part
+v=0
+z=0
+    for i in xyz:
+        if i%2==0:
+            v=v+1
+        else:
+            z=z+1
+print(v)
+print(z)
